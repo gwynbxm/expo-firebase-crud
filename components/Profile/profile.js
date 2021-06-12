4a5b71b1f4f9da435201cd80b873522661c2b70e
Gwyn Bong <gwynbong@gmail.com>
2021-06-12 13:39:28 +0800

    select photo from library and display

--- components/Profile/profile.js
+++ components/Profile/profile.js
@@ -6,7 +6,7 @@ import React, { useState, useEffect } from "react";
 import {
   Alert,
   ImageBackground,
-  ImagePicker,
+  // ImagePicker,
   StyleSheet,
   Text,
   TextInput,
@@ -14,13 +14,9 @@ import {
   View,
 } from "react-native";
 
+import * as ImagePicker from "expo-image-picker";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-// previous version
-// import { firebaseApp } from "../../config.js";
-
-//testing version
-// import firebase from "firebase/app";
 import firebase from "../../config.js";
 
 export default function Profile() {
@@ -28,8 +24,12 @@ export default function Profile() {
     firstName: "",
     email: "",
     username: "",
+    profileUrl: "",
   });
   const [errorText, setErrorText] = useState("");
+  const [image, setImage] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [transferred, setTransferred] = useState(0);
 
   const handleChangeText = (name, value) => {
     setUserData({ ...userData, [name]: value });
@@ -45,12 +45,19 @@ export default function Profile() {
           firstName: snapshot.val().firstName,
           username: snapshot.val().username,
           email: snapshot.val().email,
+          profileUrl: snapshot.val().profileUrl,
         });
       });
     return () => firebase.database().ref("/Accounts").off("value", onChildAdd);
   }, ["-MbuZgTDSHv8D-W65UBa"]);
 
   const updateUser = async () => {
+    let imgURL = await uploadImage();
+
+    if (imgURL == null && userData.profileUrl) {
+      imgURL = userData.profileUrl;
+    }
+
     firebase
       .database()
       .ref("Accounts/" + "-MbuZgTDSHv8D-W65UBa")
@@ -58,6 +65,7 @@ export default function Profile() {
         firstName: userData.firstName,
         email: userData.email,
         username: userData.username,
+        profileUrl: userData.profileUrl,
       });
     alert("updated user details!");
   };
@@ -74,6 +82,7 @@ export default function Profile() {
           firstName: userData.firstName,
           email: userData.email,
           username: userData.username,
+          profileUrl: userData.profileUrl,
         });
       alert("saved to database!");
     }
@@ -84,15 +93,47 @@ export default function Profile() {
     alert("deleted user");
   };
 
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== "web") {
+        const { status } =
+          await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== "granted") {
+          alert("Sorry, we need camera roll permissions to make this work!");
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => {}}>
+      <TouchableOpacity onPress={pickImage}>
         <View style={styles.dpContainer}>
           <ImageBackground
             style={styles.profileDp}
             imageStyle={{ borderRadius: 100 }}
             source={{
-              uri: "https://lh3.googleusercontent.com/znytw2BkzEYJv20Xp-0f8TMaopbemaVWLiRirBZ217Hxuo5vgX-KAKZDLdY3VojsYWXvfm5ollyUiAvecuwdNqzJ",
+              uri: image
+                ? image
+                : userData
+                ? userData.profileUrl ||
+                  "https://lh5.googleusercontent.com/-b0PKyNuQv5s/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclxAM4M1SCBGAO7Rp-QP6zgBEUkOQ/s96-c/photo.jpg"
+                : "https://lh3.googleusercontent.com/znytw2BkzEYJv20Xp-0f8TMaopbemaVWLiRirBZ217Hxuo5vgX-KAKZDLdY3VojsYWXvfm5ollyUiAvecuwdNqzJ",
             }}
           >
             <View style={styles.cameraContainer}>
@@ -227,6 +268,45 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   createBtnTitle: {
+    color: "white",
+  },
+
+  panel: {
+    padding: 20,
+    backgroundColor: "#FFFFFF",
+    paddingTop: 20,
+    width: "100%",
+  },
+  panelHeader: {
+    alignItems: "center",
+  },
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#00000040",
+    marginBottom: 10,
+  },
+  panelTitle: {
+    fontSize: 27,
+    height: 35,
+  },
+  panelSubtitle: {
+    fontSize: 14,
+    color: "gray",
+    height: 30,
+    marginBottom: 10,
+  },
+  panelButton: {
+    padding: 13,
+    borderRadius: 10,
+    backgroundColor: "#2e64e5",
+    alignItems: "center",
+    marginVertical: 7,
+  },
+  panelButtonTitle: {
+    fontSize: 17,
+    fontWeight: "bold",
     color: "white",
   },
 });
